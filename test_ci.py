1386e5380fb307e23c8689b771fdd02a06df4cc8
nikki xie <nikki099@gmail.com>
2025-03-08 06:24:08 +0000

    added test_ci.py

--- test_ci.py
+++ test_ci.py
@@ -1,8 +1,10 @@
 # test_ci.py
 
+
 def greet(name: int) -> str:
-    print("Hello, " + name + "!")
-    return 42
+    print("Hello, " + str(name) + "!")
+    return "42"
 
 
-greet("world")
+if __name__ == "__main__":
+    greet(2)
